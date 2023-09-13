3cbd967118cd315ee5508bdf83324cce0eb07577
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-09-13 14:09:22 -0300

    Cadastro de usuário - Estrutura visual da home

---
+++ src/pages/home/styles.ts
@@ -0,0 +1,39 @@
+import { styled, Heading, Text } from '@ignite-ui/react'
+
+export const Container = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '$20',
+
+  maxWidth: 'calc(100vw - ((100vw - 1160px) / 2))',
+  height: '100vh',
+
+  marginLeft: 'auto',
+})
+
+export const Hero = styled('div', {
+  maxWidth: 480,
+
+  padding: '0 $10',
+
+  [`${Heading}`]: {
+    '@media(max-width: 600px)': {
+      fontSize: '$6xl',
+    },
+  },
+
+  [`${Text}`]: {
+    maskType: '$2',
+    color: '$gray200',
+  },
+})
+
+export const Preview = styled('div', {
+  overflow: 'hidden',
+
+  paddingRight: '$8',
+
+  '@media(max-width: 600px)': {
+    display: 'none',
+  },
+})
