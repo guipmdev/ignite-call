2c3e685d20ea7812d0d6f61f0bca8029c4fd44ed
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-09-15 10:07:53 -0300

    Conexão com calendário - Conteúdo da sessão do Next Auth

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -44,6 +44,13 @@ export function buildNextAuthOptions(
 
         return true
       },
+
+      async session({ session, user }) {
+        return {
+          ...session,
+          user,
+        }
+      },
     },
   }
 }
